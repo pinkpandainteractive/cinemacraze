eae7ae4c97fb2362beaa46d9a9e86402daba1dfe
TobRojekt <74661399+TobRojekt@users.noreply.github.com>
2023-04-23 11:04:02 +0000

    removed obvious doc-comments

--- CinemaCraze/Assets/Scripts/Menu/MainMenu.cs
+++ CinemaCraze/Assets/Scripts/Menu/MainMenu.cs
@@ -3,24 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-/// <summary>
-/// MainMenu-Behavior
-/// </summary>
 public class MainMenu : MonoBehaviour
 {
-
-    /// <summary>
-    /// Loads the GameScene
-    /// </summary>
     public void PlayGame()
     {
         Debug.Log("Loading Game");
         SceneManager.LoadScene("Game");
     }
 
-    /// <summary>
-    /// Stops the Application
-    /// </summary>
     public void QuitGame()
     {
         Debug.Log("Quitting Game");
