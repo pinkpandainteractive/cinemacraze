841ff650f1309df7a1afee0d93d55eed3906b61b
Mas2606 <mashdo1234@gmail.com>
2023-04-23 21:10:37 +0200

    Blockout + Merge
    Blockout erstellt und gemerged

--- CinemaCraze/Assets/Scripts/Menu/MainMenu.cs
+++ CinemaCraze/Assets/Scripts/Menu/MainMenu.cs
@@ -8,7 +8,7 @@ public class MainMenu : MonoBehaviour
     public void PlayGame()
     {
         Debug.Log("Loading Game");
-        SceneManager.LoadScene("Game");
+        SceneManager.LoadScene("Blockout");
     }
 
     public void QuitGame()
