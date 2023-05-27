69a4bf6ad4974ae2f12846113aad51409f05402f
Waldemar <waldemar.ju98@gmail.com>
2023-05-27 15:53:38 +0200

    Updated Order Script

--- CinemaCraze/Assets/Scripts/Game/Order.cs
+++ CinemaCraze/Assets/Scripts/Game/Order.cs
@@ -6,70 +6,113 @@ public class Order : MonoBehaviour
 {
    
     public MainMenu mainMenu;
-    
+
     public List<string> GenerateOrder()
     {
-        List<string> listOrder = new ();
-               
-                if (Random.Range(0, 10) > 5)
-                {
-                    listOrder.Clear();
-                    int numPopcorn = Random.Range(1, 4);
-                    listOrder.Add("Popcorn x" + numPopcorn);
-                    return listOrder;
-                }
-                else if (Random.Range(0, 10) > 5)
-                {
-                    listOrder.Clear();
-                    int numNacho = Random.Range(1, 4);
-                    listOrder.Add("Nacho x" + numNacho);
-                    return listOrder;
-                }
-                else if (Random.Range(0, 10) > 5)
-                {
-                    listOrder.Clear();
-                    int numNacho = Random.Range(1, 3);
-                    int numPopcorn = Random.Range(1, 3);
+        List<string> listOrder = new List<string>();
 
-                    listOrder.Add("Nacho x" + numNacho);
-                    listOrder.Add("Popcorn x" + numPopcorn);
-                    listOrder.Sort();
-                    return listOrder;
-                }
-                else if (Random.Range(0, 10) > 5)
-                {
-                    listOrder.Clear();
-                    int numSoda = Random.Range(1, 3);
-                    int numPopcorn = Random.Range(1, 3);
+        float currentTime = Time.time; // Aktuelle Zeit seit dem Start des Spiels in Sekunden
 
-                    listOrder.Add("Soda x" + numSoda);
-                    listOrder.Add("Popcorn x" + numPopcorn);
-                    listOrder.Sort();
-                    return listOrder;
-                }
-                else if (Random.Range(0, 10) > 5)
-                {
-                    listOrder.Clear();
-                    int numNacho = Random.Range(1, 3);
-                    int numSoda = Random.Range(1, 3);
+        if (currentTime < 120f) // Zeitabhängigkeit für die ersten zwei Minuten
+        {
+            // Einfache Bestellungen häufiger
+            if (Random.Range(0, 10) > 5)
+            {
+                int numPopcorn = Random.Range(1, 4);
+                listOrder.Add("Popcorn x" + numPopcorn);
+            }
+            else if (Random.Range(0, 10) > 5)
+            {
+                int numNacho = Random.Range(1, 4);
+                listOrder.Add("Nacho x" + numNacho);
+            }
+            else if (Random.Range(0, 10) > 5)
+            {
+                int numNacho = Random.Range(1, 3);
+                int numPopcorn = Random.Range(1, 3);
+                listOrder.Add("Nacho x" + numNacho);
+                listOrder.Add("Popcorn x" + numPopcorn);
+                listOrder.Sort();
+            }
+            else if (Random.Range(0, 10) > 5)
+            {
+                int numSoda = Random.Range(1, 3);
+                int numPopcorn = Random.Range(1, 3);
+                listOrder.Add("Soda x" + numSoda);
+                listOrder.Add("Popcorn x" + numPopcorn);
+                listOrder.Sort();
+            }
+            else if (Random.Range(0, 10) > 5)
+            {
+                int numNacho = Random.Range(1, 3);
+                int numSoda = Random.Range(1, 3);
+                listOrder.Add("Nacho x" + numNacho);
+                listOrder.Add("Soda x" + numSoda);
+                listOrder.Sort();
+            }
+            else
+            {
+                int numNacho = Random.Range(1, 3);
+                int numPopcorn = Random.Range(1, 3);
+                int numSoda = Random.Range(1, 3);
+                listOrder.Add("Nacho x" + numNacho);
+                listOrder.Add("Popcorn x" + numPopcorn);
+                listOrder.Add("Soda x" + numSoda);
+                listOrder.Sort();
+            }
+        }
+        else
+        {
+            // Komplexere Bestellungen häufiger
+            if (Random.Range(0, 10) > 5)
+            {
+                int numNacho = Random.Range(1, 3);
+                int numPopcorn = Random.Range(1, 3);
+                int numSoda = Random.Range(1, 3);
+                listOrder.Add("Nacho x" + numNacho);
+                listOrder.Add("Popcorn x" + numPopcorn);
+                listOrder.Add("Soda x" + numSoda);
+                listOrder.Sort();
+            }
+            else if (Random.Range(0, 10) > 5)
+            {
+                int numNacho = Random.Range(1, 3);
+                int numSoda = Random.Range(1, 3);
+                listOrder.Add("Nacho x" + numNacho);
+                listOrder.Add("Soda x" + numSoda);
+                listOrder.Sort();
+            }
+            else if (Random.Range(0, 10) > 5)
+            {
+                int numSoda = Random.Range(1, 3);
+                int numPopcorn = Random.Range(1, 3);
+                listOrder.Add("Soda x" + numSoda);
+                listOrder.Add("Popcorn x" + numPopcorn);
+                listOrder.Sort();
+            }
+            else if (Random.Range(0, 10) > 5)
+            {
+                int numNacho = Random.Range(1, 3);
+                int numPopcorn = Random.Range(1, 3);
+                listOrder.Add("Nacho x" + numNacho);
+                listOrder.Add("Popcorn x" + numPopcorn);
+                listOrder.Sort();
+            }
+            else if (Random.Range(0, 10) > 5)
+            {
+                int numNacho = Random.Range(1, 4);
+                listOrder.Add("Nacho x" + numNacho);
+            }
+            else if (Random.Range(0, 10) > 5)
+            {
+                int numPopcorn = Random.Range(1, 4);
+                listOrder.Add("Popcorn x" + numPopcorn);
+            }
+        }
 
-                    listOrder.Add("Nacho x" + numNacho);
-                    listOrder.Add("Soda x" + numSoda);
-                    listOrder.Sort();
-                    return listOrder;
-                }
-                else
-                {
-                    listOrder.Clear();
-                    int numNacho = Random.Range(1, 3);
-                    int numPopcorn = Random.Range(1, 3);
-                    int numSoda = Random.Range(1, 3);
-                    listOrder.Add("Nacho x" + numNacho);
-                    listOrder.Add("Popcorn x" + numPopcorn);
-                    listOrder.Add("Soda x" + numSoda);
-                    listOrder.Sort();
-                    return listOrder;
-                }
+        return listOrder;
     }
+
     
+
 }
