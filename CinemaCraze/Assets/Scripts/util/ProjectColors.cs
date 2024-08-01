5dc2d2745f347f5808d6fe2df039e4d945e6199a
waldemar10 <waldemar.ju98@gmail.com>
2024-08-01 15:07:22 +0200

    ProjectColor

---
+++ CinemaCraze/Assets/Scripts/util/ProjectColors.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UnityEngine;
+
+    public class ProjectColors
+    {
+        public static readonly Color GREEN = new Color(0.0f, 1.0f, 0.0f, 1.0f);
+        public static readonly Color BLUE = new Color(0.0f, 0.0f, 1.0f, 1.0f);
+        public static readonly Color RED = new Color(1.0f, 0.0f, 0.0f, 1.0f);
+        public static readonly Color YELLOW = new Color(1.0f, 1.0f, 0.0f, 1.0f);
+        public static readonly Color WHITE = new Color(1.0f, 1.0f, 1.0f, 1.0f);
+        public static readonly Color BLACK = new Color(0.0f, 0.0f, 0.0f, 1.0f);
+        public static readonly Color ORANGE = new Color(1.0f, 0.5f, 0.0f, 1.0f);
+        public static readonly Color PURPLE = new Color(0.5f, 0.0f, 0.5f, 1.0f);
+        public static readonly Color PINK = new Color(1.0f, 0.0f, 1.0f, 1.0f);
+        public static readonly Color BROWN = new Color(0.5f, 0.25f, 0.0f, 1.0f);
+        public static readonly Color GREY = new Color(0.5f, 0.5f, 0.5f, 1.0f);
+        public static readonly Color LIGHT_BLUE = new Color(0.0f, 1.0f, 1.0f, 1.0f);
+        public static readonly Color LIGHT_GREEN = new Color(0.5f, 1.0f, 0.5f, 1.0f);
+        public static readonly Color LIGHT_RED = new Color(1.0f, 0.5f, 0.5f, 1.0f);
+        public static readonly Color LIGHT_YELLOW = new Color(1.0f, 1.0f, 0.5f, 1.0f);
+        public static readonly Color LIGHT_ORANGE = new Color(1.0f, 0.75f, 0.5f, 1.0f);
+        public static readonly Color LIGHT_PURPLE = new Color(0.75f, 0.5f, 0.75f, 1.0f);
+        public static readonly Color LIGHT_PINK = new Color(1.0f, 0.5f, 0.75f, 1.0f);
+        public static readonly Color LIGHT_BROWN = new Color(0.75f, 0.5f, 0.25f, 1.0f);
+        public static readonly Color LIGHT_GREY = new Color(0.75f, 0.75f, 0.75f, 1.0f);
+        public static readonly Color DARK_BLUE = new Color(0.0f, 0.0f, 0.5f, 1.0f);
+        public static readonly Color DARK_GREEN = new Color(0.0f, 0.5f, 0.0f, 1.0f);
+        public static readonly Color DARK_RED = new Color(0.5f, 0.0f, 0.0f, 1.0f);
+        public static readonly Color DARK_YELLOW = new Color(0.5f, 0.5f, 0.0f, 1.0f);
+        public static readonly Color DARK_ORANGE = new Color(0.5f, 0.25f, 0.0f, 1.0f);
+        public static readonly Color DARK_PURPLE = new Color(0.25f, 0.0f, 0.25f, 1.0f);
+        public static readonly Color DARK_PINK = new Color(0.5f, 0.0f, 0.5f, 1.0f);
+        public static readonly Color DARK_BROWN = new Color(0.25f, 0.0f, 0.0f, 1.0f);
+        public static readonly Color DARK_GREY = new Color(0.25f, 0.25f, 0.25f, 1.0f);
+    }
